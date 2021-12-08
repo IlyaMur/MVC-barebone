48ee00e065fc2c26dda31e66b7368a5b20842e12
Ilya Mur <ilyamur@hotmail.com>
2021-12-08 15:46:43 +0300

    add regexp to router

--- Core/Router.php
+++ Core/Router.php
@@ -17,16 +17,32 @@ class Router
         return $this->params;
     }
 
-    public function add(string $route, array $params): void
+    public function add(string $route, array $params = []): void
     {
+        // convert route to regexp
+        $route = preg_replace('/\//', '\\/', $route);
+
+        // convert variables
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+
+        $route = '/^' . $route . '$/i';
+
         $this->routes[$route] = $params;
     }
 
     public function match(string $url): bool
     {
         foreach ($this->routes as $route => $params) {
-            if ($url === $route) {
+            if (preg_match($route, $url, $matches)) {
+                var_dump($matches);
+                foreach ($matches as $key => $match) {
+                    if (is_string($key)) {
+                        $params[$key] = $match;
+                    }
+                }
+
                 $this->params = $params;
+
                 return true;
             }
         }
--- public/index.php
+++ public/index.php
@@ -18,8 +18,14 @@ $router = new Router();
 
 $router->add(route: '', params: ['controller' => 'Home', 'action' => 'index']);
 $router->add(route: 'posts', params: ['controller' => 'Posts', 'action' => 'index']);
-$router->add(route: 'posts/new', params: ['controller' => 'Posts', 'action' => 'new']);
+// $router->add(route: 'posts/new', params: ['controller' => 'Posts', 'action' => 'new']);
 
+$router->add(route: '{controller}/{action}');
+$router->add(route: 'admin/{controller}/{action}');
+
+echo '<pre>';
+var_dump($router->getRoutes());
+echo '</pre>';
 $url = $_SERVER['QUERY_STRING'];
 
 if ($router->match($url)) {
