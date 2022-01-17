d0e7a70a76ad301f96b1873a5008e4744f7d1d20
Ilya Mur <ilyamur@hotmail.com>
2022-01-17 03:07:33 +0300

    change const to define in config file

--- config/config.php
+++ config/config.php
@@ -3,10 +3,17 @@
 declare(strict_types=1);
 
 // db credentials
-const DB_HOST = 'localhost';
-const DB_NAME = 'mvc';
-const DB_USER = 'root';
-const DB_PASSWORD = '';
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'mvc');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
 
 // showing additional errors info
 const SHOW_ERRORS = true;
+
+// errors settings
+error_reporting(E_ALL);
+
+set_error_handler('Ilyamur\PhpOnRails\Error::errorHandler');
+set_exception_handler('Ilyamur\PhpOnRails\Error::exceptionHandler');
